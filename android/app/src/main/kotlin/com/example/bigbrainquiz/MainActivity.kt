b8d8bb42b3e732291959964e4acbe379c3774244
Sameer <sameer.w@somaiya.edu>
2021-12-02 11:49:08 +0530

    pre-firebase commit

---
+++ android/app/src/main/kotlin/com/example/bigbrainquiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bigbrainquiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bigbrainquiz/ui/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Open Trivia',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.indigo,
+          fontFamily: "Montserrat",
+          buttonColor: Colors.pink,
+          buttonTheme: ButtonThemeData(
+              buttonColor: Colors.pink,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              textTheme: ButtonTextTheme.primary)),
+      home: HomePage(),
+    );
+  }
+}
